3a064d45e75957773b378f8f7b380619aa98fe40
Isamyrat <kurbanowisa07@gmail.com>
2021-08-26 15:31:03 +0300

    add demo

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      <a className="grey-text text-lighten-4 right" href="https://github.com/Isamyrat/movies_small_project_react">More Links</a>
       </div>
     </div>
   </footer>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header(){
         return    <nav className = " purple lighten-2">
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">React Movies</a>
+          <a href="/" className="brand-logo">React Movies</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#">Repo</a></li>
+            <li><a href="https://github.com/Isamyrat/movies_small_project_react">Repo</a></li>
            
           </ul>
         </div>
